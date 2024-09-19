1ca7948c9b61adaa1589b14fabb043afd71eba60
felinibertomcfalso <136160356+BeCuber@users.noreply.github.com>
2024-09-19 21:02:08 +0200

    Add button and display-text

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 document.getElementById("mostrarMensaje").addEventListener("click", function() {
     var mensaje = document.getElementById("mensaje");
     mensaje.style.display = "block"; // Muestra el mensaje
+    var foto = document.getElementById("foto");
+    foto.style.display = "block"; // Muestra la imagen
 });
