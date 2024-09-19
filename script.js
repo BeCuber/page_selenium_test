b94563b1afbe42d84ad6c29e8c9f4d5f52c430aa
felinibertomcfalso <136160356+BeCuber@users.noreply.github.com>
2024-09-19 20:37:19 +0200

    Add button and display-text

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("mostrarMensaje").addEventListener("click", function() {
+    var mensaje = document.getElementById("mensaje");
+    mensaje.style.display = "block"; // Muestra el mensaje
+});
